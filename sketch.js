a73406401d3dc003947c9ecc4757c8d828eb5c97
cnkahyaoglu <cnkahyaoglu@gmail.com>
2018-05-14 15:34:47 +0200

    wut

--- sketch.js
+++ sketch.js
@@ -8,14 +8,18 @@ var osc, fft;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  frameRate(200);
+
 	capture = createCapture(VIDEO);
-	capture.size(windowWidth, windowHeight);
-  smallPoint = 10;
-  largePoint = 100;
+	capture.size(windowWidth/2, windowHeight/2);
+  smallPoint = 4;
+  largePoint = 40;
   imageMode(CENTER);
   noStroke();
   background(255);
   capture.loadPixels();
+
+
 
   osc = new p5.TriOsc(); // set frequency and type
   osc.amp(0.5);
@@ -27,7 +31,7 @@ function setup() {
 
 function draw() {
 
-  var pointillize = map(mouseX, 0, width, smallPoint, largePoint);
+    var pointillize = map(mouseX, 0, width, smallPoint, largePoint);
   	var x = floor(random(capture.width));
   	var y = floor(random(capture.height));
 
@@ -40,14 +44,35 @@ function draw() {
 	  var k = floor(random(capture.width));
   	var l = floor(random(capture.height));
 
-	var aa = floor(random(capture.width));
+	  var aa = floor(random(capture.width));
   	var bb = floor(random(capture.height));
 
-	var kk = floor(random(capture.width));
+	  var kk = floor(random(capture.width));
   	var ll = floor(random(capture.height));
 
-	var qq = floor(random(capture.width));
+	  var qq = floor(random(capture.width));
   	var ww = floor(random(capture.height));
+
+    var xx = floor(random(capture.width));
+  	var yy = floor(random(capture.height));
+
+	  var qqq = floor(random(capture.width));
+ 		var www = floor(random(capture.height));
+
+	  var aaa = floor(random(capture.width));
+  	var bbb = floor(random(capture.height));
+
+	  var kkk = floor(random(capture.width));
+  	var lll = floor(random(capture.height));
+
+	  var aaaa = floor(random(capture.width));
+  	var bbbb = floor(random(capture.height));
+
+	  var kkkk = floor(random(capture.width));
+  	var llll = floor(random(capture.height));
+
+	  var qqqq = floor(random(capture.width));
+  	var wwww = floor(random(capture.height));
 
 	stroke(0)
 
@@ -77,11 +102,38 @@ function draw() {
 
 	var pix7 = capture.get(kk, ll);
   fill(pix7, 128);
-  rect(k, l, pointillize, pointillize);
+  rect(kk, ll, pointillize, pointillize);
 
-  var freq = map(mouseX, 0, width, 880, 40);
+  var pix8 = capture.get(qqq, www);
+  fill(pix8, 128);
+  rect(qqq, www, pointillize, pointillize);
+
+	var pix9 = capture.get(aaa, bbb);
+  fill(pix9, 128);
+  rect(aaa, bbb, pointillize, pointillize);
+
+	var pix10 = capture.get(kkk, lll);
+  fill(pix10, 128);
+  rect(kkk, lll, pointillize, pointillize);
+
+  var pix11 = capture.get(xx, yy);
+  fill(pix11, 128);
+  rect(xx, yy, pointillize, pointillize);
+
+  var pix12 = capture.get(aaaa, bbbb);
+  fill(pix12, 128);
+  rect(aaaa, bbbb, pointillize, pointillize);
+
+	var pix13 = capture.get(kkkk, llll);
+  fill(pix13, 128);
+  rect(kkkk, llll, pointillize, pointillize);
+
+  var pix14 = capture.get(qqqq, wwww);
+  fill(pix14, 128);
+  rect(qqqq, wwww, pointillize, pointillize);
+
+  var freq = map(mouseX, 0, width, 100, 300);
   osc.freq(freq);
 
-  var amp = map(mouseY, 0, height, 0.01, 1);
-  osc.amp(amp);
+
 }
